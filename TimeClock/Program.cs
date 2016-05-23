9659e4e3624f7985a919a93be6347aef6ad145b2
Tim Etchells <tetchel@gmail.com>
2016-05-23 14:41:32 -0400

    first

---
+++ TimeClock/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace TimeClock {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.Run(new TimeClockContext());
+        }
+    }
+
+    public class TimeClockContext : ApplicationContext {
+        private NotifyIcon ni;
+        private DateTime start;
+        private MenuItem timer;
+
+        private const int POLL_FREQUENCY = 10;          //in seconds, how often to check if user is not locked
+                                                        //decrease for increased precision and cpu usage
+        private const int NOTIFY_FREQUENCY = 1;         //number of clock ticks that must pass before user is notified
+
+        private const string time_format = @"hh\:mm\:ss";
+
+        //counts POLLs
+        private int clock = 0;
+        //toggled when user locks/unlocks workstation
+        private bool locked = false;
+
+        public TimeClockContext() {
+            start = DateTime.Now;
+            timer = new MenuItem();
+            ContextMenu cm = new ContextMenu(new MenuItem[] { timer, new MenuItem("Open", mainApp), new MenuItem("Exit", Exit) });
+            cm.Popup += onPopUp;
+
+            ni = new NotifyIcon() {
+                Icon = Properties.Resources.AppIcon,
+                ContextMenu = cm,
+                Visible = true
+            };
+            ni.Click += mainApp;
+
+            //locked workstation listener
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+
+            //thread that sleeps and then updates user every NOTIFY_FREQUENCY
+            Thread loopthread = new Thread(new ThreadStart(loop));
+            loopthread.Start();
+        }
+
+        //loop eternally, updating time every minute
+        //needs to be done in a separate thread so that UI thread still works
+        void loop() {
+            while (true) {
+                Thread.Sleep(POLL_FREQUENCY * 1000);
+                //only count time if !locked
+                if(!locked) {
+                    clock++;
+                }
+
+                //notify when locked
+                if (clock % NOTIFY_FREQUENCY == 0) {
+                    ni.BalloonTipTitle = "Greetings";
+                    ni.BalloonTipText = "You have been working for " + getElapsed();
+                    ni.ShowBalloonTip(3000);
+                }
+            }
+        }
+
+        //called when context menu is opened
+        void onPopUp(object sender, EventArgs e) {
+            timer.Text = getElapsed();
+        }
+
+        void mainApp(object sender, EventArgs e) {
+            //TODO update while dialog is open (even better, when context menu is open too)
+            MessageBox.Show("You clocked in at " + start.ToString(time_format) + "\n" +
+                            "Working for: " + getElapsed());
+        }
+
+        //calculates the time elapsed and returns as string
+        string getElapsed() {
+           return DateTime.Now.Subtract(start).ToString(time_format);
+        }
+
+        private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e) {
+            if (e.Reason == SessionSwitchReason.SessionLock) {
+                locked = true;
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock) {
+                locked = false;
+            }
+        }
+
+        //exit application
+        void Exit(object sender, EventArgs e) {
+            ni.Visible = false;
+            Application.Exit();
+        }
+    }
+}
